a51a5fa6a319551843f7ce7df4eee873b7410eb6
logisticsrumah-beep <logisticsrumah@gmail.com>
2026-02-26 09:17:55 +0300

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_GOOGLE_SCRIPT_URL: string;
+  // Agar yahan GEMINI_API_KEY likha hai to usey mita dein
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
